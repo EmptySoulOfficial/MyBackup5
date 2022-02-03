f6db49f27370b56bfcf19915bb28722706b64c50
EmptySoulOfficial <steveemptysoul.official@gmail.com>
2022-02-03 20:57:34 +0100

    icons

--- src/components/navigation/navItem/navItem.jsx
+++ src/components/navigation/navItem/navItem.jsx
@@ -3,7 +3,7 @@ import './navItem.css'
 import Icon from '../../../assets/js/icon.asset.jsx'
 import classNames from 'classnames'
 
-function NavItem({label, iconSrc, iconAlt, navitemdefaultselected, navItemId}) {
+function NavItem({label, iconSrc, iconAlt, navitemdefaultselected, navItemId, iconName}) {
 
   const [tnavItemActive, tsetnavItemActive] = useState(false);
   return (
@@ -11,7 +11,7 @@ function NavItem({label, iconSrc, iconAlt, navitemdefaultselected, navItemId}) {
   
     <div id={navItemId} className={classNames('nav-item', {'nav-item--active': navitemdefaultselected, "" : !navitemdefaultselected })} onClick={() => tsetnavItemActive(!tnavItemActive)}>
         <div className="nav-item_icon">
-        <Icon name="logo_test" color="var(--lightgray)" size={30} />
+        <Icon name={iconName} color="var(--lightgray)" size={30} />
         </div>
         <label className="nav-item_label">{label}</label>
         <div className="nav-item_bar"></div>
--- src/components/navigation/navigation.jsx
+++ src/components/navigation/navigation.jsx
@@ -25,12 +25,12 @@ function Navigation({blur}) {
         </div> 
         <div className="navigation_container">
             <div className="item-container">
-                <NavItem label={lang_text.menulabel_backup}  navItemId={"ni_backup"} iconAlt="create backup icon" navitemdefaultselected={false}/>
-                <NavItem label={lang_text.menulabel_restore} navItemId={"ni_restore"} iconAlt="restore icon" navitemdefaultselected={false}/>
-                <NavItem label={lang_text.menulabel_manage}  navItemId={"ni_options"} iconAlt="manage backup icon" navitemdefaultselected={false}/>
+                <NavItem label={lang_text.menulabel_backup}  navItemId={"ni_backup"} iconName={"backup"} iconAlt="create backup icon" navitemdefaultselected={false}/>
+                <NavItem label={lang_text.menulabel_restore} navItemId={"ni_restore"} iconName={"restore"} iconAlt="restore icon" navitemdefaultselected={false}/>
+                <NavItem label={lang_text.menulabel_manage}  navItemId={"ni_options"} iconName={"options"} iconAlt="manage backup icon" navitemdefaultselected={false}/>
             </div>
             <div className="config-button_container">
-            <NavItem label={lang_text.menulabel_config}  navItemId={"ni_config"} navitemdefaultselected={false} iconAlt="manage backup icon"/>
+            <NavItem label={lang_text.menulabel_config}  navItemId={"ni_config"} iconName={"config"} navitemdefaultselected={false} iconAlt="manage backup icon"/>
             </div>
         </div>
     </nav>
