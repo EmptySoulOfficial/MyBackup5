b81bcbf9ca8dfc447a71986554261fa45ebe46b7
Steve <stefan.pietrowski@prime-force.com>
2023-11-29 09:14:31 +0100

    ToDos, Parse Data, User Data Header Templates

--- src/core/DefaultData/ParseDefaultData.js
+++ src/core/DefaultData/ParseDefaultData.js
@@ -1,9 +1,12 @@
 //Parse default data
 import * as newBackupItem from './data/NewBackupItem.json'
 import * as themeData from './data/themes.json'
+import * as ubaHeader from './data/mb1Templates/UBAHeader.json'
 
 const newBackupData = JSON.parse(JSON.stringify(newBackupItem)).default
 const jsonObjThemes = JSON.parse(JSON.stringify(themeData)).default
+const jsonUbaHeaderTemplate = JSON.parse(JSON.stringify(ubaHeader)).default
 
 export const getNewBackupData = () => newBackupData
 export const FactoryThemeData = () => jsonObjThemes
+export const Template_UBAHeader = () => jsonUbaHeaderTemplate
